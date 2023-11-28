82ed66827ce4d232606c75a0393a2765c11c0743
Will-Martinez <willandersonmartinez@gmail.com>
2023-11-27 22:57:17 -0300

    Refactoring create product class code.

--- Server/src/API/Services/Products/CreateProduct.ts
+++ Server/src/API/Services/Products/CreateProduct.ts
@@ -6,185 +6,19 @@ import TechnicalMaterialFactory from "./ConcreteFactories/TechnicalMaterialFacto
 import FineStationaryFactory from "./ConcreteFactories/FineStationaryFactory";
 import OrganizationFactory from "./ConcreteFactories/OrganizationFactory";
 import WritingCorrectionFactory from "./ConcreteFactories/WritingCorrectionFactory";
+import StationaryFactory from "./Interfaces/StationaryFactory";
 
 export default class CreateProduct {
 
-    private createSchoolSupply(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const schoolSupplyFactory: SchoolSupplyFactory = new SchoolSupplyFactory();
-        return schoolSupplyFactory.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-    }
-
-    private createOfficeSupply(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const officeSupplyFactory: OfficeSupplyFactory = new OfficeSupplyFactory();
-        return officeSupplyFactory.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-    }
-
-    private createArtsCrafts(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const artsCraftFactory: ArtsCraftFactory = new ArtsCraftFactory();
-        return artsCraftFactory.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-    }
-
-    private createTechnicalMaterial(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const technicalMaterialFactory: TechnicalMaterialFactory = new TechnicalMaterialFactory();
-        return technicalMaterialFactory.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-    }
-
-    private createFineStationary(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const fineStationaryFactory: FineStationaryFactory = new FineStationaryFactory();
-        return fineStationaryFactory.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-    }
-
-    private createOrganization(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const organizationFactory: OrganizationFactory = new OrganizationFactory();
-        return organizationFactory.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-    }
-
-    private createWritingCorrection(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): StationaryProduct {
-        const writingCorrection: WritingCorrectionFactory = new WritingCorrectionFactory();
-        return writingCorrection.createProduct(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        )
-    }
-
-    private defineProductCreation(
-        category: string,
-        productName: string,
-        amount: number,
-        price: number,
-        available: boolean
-    ): object {
+    private defineProductCreation(): object {
         return {
-            "School Supply": this.createSchoolSupply(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            ),
-            "Office Supply": this.createOfficeSupply(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            ),
-            "Arts and Crafts": this.createArtsCrafts(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            ),
-            "Technical Material": this.createTechnicalMaterial(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            ),
-            "Fine Stationery": this.createFineStationary(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            ),
-            "Organization": this.createOrganization(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            ),
-            "Writing and Correction": this.createWritingCorrection(
-                category,
-                productName,
-                amount,
-                price,
-                available
-            )
+            "School Supply": new SchoolSupplyFactory() ,
+            "Office Supply": new OfficeSupplyFactory,
+            "Arts and Crafts": new ArtsCraftFactory,
+            "Technical Material": new TechnicalMaterialFactory,
+            "Fine Stationery": new FineStationaryFactory(),
+            "Organization": new OrganizationFactory(),
+            "Writing and Correction": new WritingCorrectionFactory()
         }
     }
 
@@ -195,13 +29,8 @@ export default class CreateProduct {
         price: number,
         available: boolean
     ): StationaryProduct {
-        const productCreation: object = this.defineProductCreation(
-            category,
-            productName,
-            amount,
-            price,
-            available
-        );
-        return productCreation[category];
+        const factoryDefinition: object = this.defineProductCreation();
+        const concreteFactory: StationaryFactory = factoryDefinition[category];
+        return concreteFactory.createProduct(category, productName, amount, price, available);
     }
 }
